b00464ca5089d523f1f8425a5440d9d7bf60a1ec
Mihir Mhatre <65181802+msmhatre18@users.noreply.github.com>
2023-03-29 20:37:15 +0000

    Fixed Program Page

--- src/components/ProgamRow/index.jsx
+++ src/components/ProgamRow/index.jsx
@@ -1,29 +1,24 @@
 import React from "react";
-
+import './../../styles/ProgramRow.css';
 import { Text, Img, Button } from "components";
 
 const ProgamRow = (props) => {
   return (
     <>
-      <div className={props.className}>
+      <div className={`program-row ${props.className}`}>
         <div className="flex md:flex-col flex-row md:gap-[20px] items-start justify-start ml-[10px] w-[99%]">
           <Text
-            className="font-inter md:mt-[0] mt-[9px] not-italic text-black_900 text-left w-[auto]"
+            className="text-container"
             as="h4"
             variant="h4"
           >
-            Program Name/ID 2 Template
+            {props.target}
           </Text>
-          <Img
-            src="images/img_arrowdown.svg"
-            className="h-[20px] md:ml-[0] ml-[10px] md:mt-[0] mt-[12px] w-[auto]"
-            onClick={props?.onClick}
-            alt="arrowdown"
-          />
-          <Button className="bg-blue_600 cursor-pointer font-inter font-normal leading-[normal] min-w-[254px] md:ml-[0] ml-[408px] not-italic py-[6px] rounded-[21px] sm:text-[20px] md:text-[22px] text-[24px] text-center text-white_A700 w-[auto]">
-            Create New
-          </Button>
         </div>
+        <div className="button-container">
+          <button className="create-button">Open Template</button>
+          <button className="create-button">Create New</button>
+      </div>
       </div>
     </>
   );
--- src/pages/ProgramsDropdownOpen/index.jsx
+++ src/pages/ProgramsDropdownOpen/index.jsx
@@ -15,6 +15,7 @@ const ProgramsDropdownOpenPage = () => {
             src="images/img_arrowup.svg"
             className="h-[33px] md:mt-[0] mt-[3px] w-[auto]"
             alt="arrowup"
+            onClick={() => navigate(-1)}
           />
           <Text
             className="md:ml-[0] ml-[415px] md:mt-[0] mt-[8px] not-italic text-black_900 text-center w-[auto]"
@@ -26,116 +27,25 @@ const ProgramsDropdownOpenPage = () => {
           <Text
             className="bg-white_A700 flex h-[40px] items-center justify-center mb-[12px] md:ml-[0] ml-[446px] not-italic outline outline-[1px] outline-black_900 rounded-[50%] text-black_900 text-center w-[40px]"
             variant="body2"
+            onClick={() => navigate("/")}
           >
             Logout
           </Text>
         </div>
-        <div className="md:h-[535px] h-[959px] md:px-[20px] relative w-[100%]">
-          <div className="absolute flex inset-x-[0] items-center justify-start mx-[auto] top-[0] w-[98%]">
-            <div className="flex md:flex-col flex-row gap-[14px] items-start justify-between w-[100%]">
-              <div
-                className="bg-cover bg-no-repeat flex md:flex-1 h-[67px] items-center justify-end p-[10px] md:w-[100%] w-[auto]"
-                style={{ backgroundImage: "url('images/img_group163.svg')" }}
-              >
-                <div className="flex md:flex-col flex-row md:gap-[40px] items-center justify-between mt-[5px] md:w-[100%] w-[99%]">
-                  <Text
-                    className="not-italic text-black_900 text-left w-[auto]"
-                    as="h4"
-                    variant="h4"
-                  >
-                    Program Name/ID 1 Template
-                  </Text>
-                  <Button className="bg-blue_600 cursor-pointer font-normal leading-[normal] min-w-[254px] not-italic py-[6px] rounded-[21px] sm:text-[20px] md:text-[22px] text-[24px] text-center text-white_A700 w-[auto]">
-                    Create New
-                  </Button>
-                </div>
-              </div>
-              <Img
-                src="images/img_scrollbar.svg"
-                className="h-[373px] md:mt-[0] mt-[15px] w-[auto]"
-                alt="scrollbar"
-              />
-            </div>
-          </div>
-          <div className="absolute bottom-[0] md:h-[535px] h-[826px] inset-x-[0] mx-[auto] w-[100%]">
-            <div className="absolute flex flex-col inset-[0] justify-center m-[auto] md:w-[100%] w-[96%]">
-              <div className="bg-blue_600 border-[2px] border-black_900 border-solid flex items-end justify-start mx-[auto] p-[14px] w-[100%]">
-                <div className="flex items-center justify-start mb-[73px] md:w-[100%] w-[2%]">
-                  <Img
-                    src="images/img_scrollbar.svg"
-                    className="h-[190px] w-[auto]"
-                    alt="scrollbar_One"
-                  />
-                </div>
-              </div>
-              <Img
-                src="images/img_screenshot20220725.png"
-                className="h-[788px] mt-[-253px] mx-[auto] object-cover w-[64%] z-[1]"
-                alt="screenshot20220"
-              />
-            </div>
-            <div className="absolute bg-white_A700 bottom-[35%] flex flex-row gap-[33px] inset-x-[0] items-start justify-start mx-[auto] p-[5px] w-[100%]">
-              <Button className="bg-transparent cursor-pointer font-normal leading-[normal] mb-[3px] min-w-[44px] ml-[32px] not-italic sm:text-[20px] md:text-[22px] text-[24px] text-black_900 text-center w-[auto]">
-                Edit
-              </Button>
-              <Button className="bg-transparent cursor-pointer font-normal leading-[normal] min-w-[47px] my-[2px] not-italic sm:text-[20px] md:text-[22px] text-[24px] text-black_900 text-center w-[auto]">
-                Add
-              </Button>
-            </div>
-            <div
-              className="absolute bg-cover bg-no-repeat bottom-[40%] flex h-[67px] inset-x-[0] items-center justify-end mx-[auto] p-[9px] w-[96%]"
-              style={{ backgroundImage: "url('images/img_group163.svg')" }}
-            >
-              <div className="flex md:flex-col flex-row md:gap-[40px] items-end justify-between mt-[7px] md:w-[100%] w-[99%]">
-                <Text
-                  className="mb-[4px] md:mt-[0] mt-[7px] not-italic text-black_900 text-left w-[auto]"
-                  as="h4"
-                  variant="h4"
-                >
-                  Program Name/ID 5 Template
-                </Text>
-                <Button className="bg-blue_600 cursor-pointer font-normal leading-[normal] min-w-[254px] not-italic py-[6px] rounded-[21px] sm:text-[20px] md:text-[22px] text-[24px] text-center text-white_A700 w-[auto]">
-                  Create New
-                </Button>
-              </div>
-            </div>
-            <div
-              className="absolute bg-cover bg-no-repeat flex h-[67px] inset-x-[0] items-center justify-end mx-[auto] p-[12px] top-[35%] w-[96%]"
-              style={{ backgroundImage: "url('images/img_group163.svg')" }}
-            >
-              <div className="flex md:flex-col flex-row md:gap-[40px] items-end justify-between md:w-[100%] w-[99%]">
-                <Text
-                  className="mb-[4px] md:mt-[0] mt-[7px] not-italic text-black_900 text-left w-[auto]"
-                  as="h4"
-                  variant="h4"
-                >
-                  Program Name/ID 3 Template
-                </Text>
-                <Button className="bg-blue_600 cursor-pointer font-normal leading-[normal] min-w-[254px] not-italic py-[6px] rounded-[21px] sm:text-[20px] md:text-[22px] text-[24px] text-center text-white_A700 w-[auto]">
-                  Create New
-                </Button>
-              </div>
-            </div>
-            <div
-              className="absolute bg-cover bg-no-repeat flex h-[67px] inset-x-[0] items-center justify-end mx-[auto] p-[12px] top-[43%] w-[96%]"
-              style={{ backgroundImage: "url('images/img_group163.svg')" }}
-            >
-              <div className="flex md:flex-col flex-row md:gap-[40px] items-end justify-between md:w-[100%] w-[99%]">
-                <Text
-                  className="mb-[4px] md:mt-[0] mt-[7px] not-italic text-black_900 text-left w-[auto]"
-                  as="h4"
-                  variant="h4"
-                >
-                  Program Name/ID 4 Template
-                </Text>
-                <Button className="bg-blue_600 cursor-pointer font-normal leading-[normal] min-w-[254px] not-italic py-[6px] rounded-[21px] sm:text-[20px] md:text-[22px] text-[24px] text-center text-white_A700 w-[auto]">
-                  Create New
-                </Button>
-              </div>
-            </div>
-          </div>
-          <ProgamRow
-            className="absolute bg-bluegray_100 flex flex-row inset-x-[0] items-center justify-start mx-[auto] outline outline-[2px] outline-black_900 p-[11px] top-[7%] sm:w-[100%] w-[96%]"
+        <div className="flex flex-col items-center justify-center mt-10 w-full md:flex-row md:gap-4 md:justify-between md:mt-20">
+        <ProgamRow
+            target = "Program 1"
+            className= "flex flex-row items-end justify-start md:ml-[0] ml-[7px] mt-[40px] md:w-[100%] w-[78%]"
+            onClick={() => navigate("/sessionviewprogramtemplate")}
+          />
+        <ProgamRow
+            target = "Program 2"
+            className="flex flex-row items-end justify-start md:ml-[0] ml-[7px] mt-[40px] md:w-[100%] w-[78%]"
+            onClick={() => navigate("/sessionviewprogramtemplate")}
+        />
+        <ProgamRow
+            target = "Program 3"
+            className="flex flex-row items-end justify-start md:ml-[0] ml-[7px] mt-[40px] md:w-[100%] w-[78%]"
             onClick={() => navigate("/sessionviewprogramtemplate")}
           />
         </div>

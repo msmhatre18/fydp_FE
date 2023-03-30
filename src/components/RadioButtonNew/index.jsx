6cbc2f7d8952b82c972ed2bfb0ac4c19b3b6f845
Mihir Mhatre <65181802+msmhatre18@users.noreply.github.com>
2023-03-30 00:34:12 +0000

    Main Program Detail Page

---
+++ src/components/RadioButtonNew/index.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './../../styles/DataCollection.css';
+import { Text, Button } from "components";
+
+function RadioButtons(props) {
+  const navigate = useNavigate();
+  
+  const [selectedValue, setSelectedValue] = useState('');
+
+  const handleRadioChange = (event) => {
+    setSelectedValue(event.target.value);
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    if (selectedValue === 'Cold Probe Collection') {
+      navigate('/ProgramDetailsEntryColdProbe');
+    } else if (selectedValue === 'Mass Trial Collection') {
+      navigate('/ProgramsDetailsEntryMassTrial');
+    } 
+  }
+
+  return (
+    <>
+      <div className={`data-collection ${props.className}`} style={{ width: '450px' }}>
+        <div className="radio-container">
+            <Text className="text-container" as="h4" variant="h4">
+                Program Type:  
+            </Text>
+            <form onSubmit={handleFormSubmit}>
+                <label className="radio-buttons">
+                    <input
+                        type="radio"
+                        value="Cold Probe Collection"
+                        checked={selectedValue === 'Cold Probe Collection'}
+                        onChange={handleRadioChange}
+                    />
+                    Cold Probe Collection
+                </label>
+                <label className="radio-buttons">
+                    <input
+                        type="radio"
+                        value="Mass Trial Collection"
+                        checked={selectedValue === 'Mass Trial Collection'}
+                        onChange={handleRadioChange}
+                    />
+                    Mass Trial Collection
+                </label>
+                <Button className="replace-button" type="submit">
+                    Go
+                </Button>
+            </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RadioButtons;
--- src/pages/ProgramDetailsEntry/index.jsx
+++ src/pages/ProgramDetailsEntry/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-
+import './../../styles/input.css';
 import { Img, Text, Input } from "components";
+import RadioButtonNew from "components/RadioButtonNew";
 import { useNavigate } from "react-router-dom";
 
 const ProgramDetailsEntryPage = () => {
@@ -26,74 +27,52 @@ const ProgramDetailsEntryPage = () => {
                 Program Name/ID 2
               </Text>
             </div>
+
+
             <div className="flex flex-col items-start justify-start md:ml-[0] ml-[64px] md:w-[100%] w-[94%]">
               <Input
-                wrapClassName="bg-white_A700 outline outline-[2px] outline-black_900 pl-[14px] pr-[35px] py-[4px] w-[28%]"
-                className="font-normal leading-[normal] md:text-[22px] not-italic p-[0] placeholder:text-bluegray_100 sm:pr-[20px] sm:text-[20px] text-[24px] text-bluegray_100 text-left w-[100%]"
+                wrapClassName="input-box"
+                className="input"
                 name="fieldCounter"
                 placeholder="Field 1"
               ></Input>
               <div className="flex md:flex-col flex-row gap-[35px] items-center justify-start mt-[43px] md:w-[100%] w-[71%]">
                 <Input
-                  wrapClassName="bg-white_A700 outline outline-[2px] outline-black_900 pl-[14px] pr-[35px] py-[4px] md:w-[100%] w-[48%]"
-                  className="font-normal leading-[normal] md:text-[22px] not-italic p-[0] placeholder:text-bluegray_100 sm:pr-[20px] sm:text-[20px] text-[24px] text-bluegray_100 text-left w-[100%]"
+                  wrapClassName="input-box"
+                  className="input"
                   name="fieldCounter_One"
                   placeholder="Field 2"
                 ></Input>
                 <Input
-                  wrapClassName="bg-white_A700 outline outline-[2px] outline-black_900 pl-[7px] pr-[35px] py-[4px] md:w-[100%] w-[48%]"
-                  className="font-normal leading-[normal] md:text-[22px] not-italic p-[0] placeholder:text-bluegray_100 sm:pr-[20px] sm:text-[20px] text-[24px] text-bluegray_100 text-left w-[100%]"
+                  wrapClassName="input-box"
+                  className="input"
                   name="fieldCounter_Two"
                   placeholder="Field 3"
                 ></Input>
               </div>
+
+
+
               <div className="bg-white_A700 flex h-[150px] items-center justify-start min-h-[] mt-[43px] outline outline-[2px] outline-black_900 p-[46px] sm:px-[20px] md:px-[40px] md:w-[100%] w-[920px]">
                 <Input
                   wrapClassName="h-[150px] max-h-[250px] min-h-[100px] min-w-[920px] relative right-[45px]"
-                  className="font-normal leading-[normal] md:text-[22px] not-italic p-[0] placeholder:text-bluegray_100 sm:text-[20px] text-[24px] text-bluegray_100 text-left w-[100%]"
+                  className="input"
                   name="fieldCounter_Three"
-                  placeholder="Field 3"
+                  placeholder="Field 4"
                 ></Input>
               </div>
-              <Input
-                wrapClassName="bg-white_A700 mt-[43px] outline outline-[2px] outline-black_900 p-[35px] w-[100%]"
-                className="font-normal leading-[normal] md:text-[22px] not-italic p-[0] placeholder:bg-white_A700 placeholder:text-bluegray_100 sm:px-[20px] sm:text-[20px] text-[24px] text-bluegray_100 text-left w-[100%]"
-                name="field_Four"
-                placeholder="Field 4"
-              ></Input>
-              <div className="flex md:flex-col flex-row md:gap-[20px] items-start justify-start mt-[71px] md:w-[100%] w-[91%]">
-                <Text
-                  className="md:mt-[0] mt-[2px] not-italic text-black_900 text-left w-[auto]"
-                  as="h4"
-                  variant="h4"
-                >
-                  Program Type
-                </Text>
-                <Text
-                  className="mb-[2px] md:ml-[0] ml-[98px] not-italic text-black_900 text-left w-[auto]"
-                  as="h4"
-                  variant="h4"
-                >
-                  Cold Probe Collection
-                </Text>
-                <Text
-                  className="mb-[2px] md:ml-[0] ml-[81px] not-italic text-black_900 text-left w-[auto]"
-                  as="h4"
-                  variant="h4"
-                >
-                  Mass Trial Collection
-                </Text>
+
+              <div className="bg-white_A700 flex h-[150px] items-center justify-start min-h-[] mt-[43px] outline outline-[2px] outline-black_900 p-[46px] sm:px-[20px] md:px-[40px] md:w-[100%] w-[920px]">
+                <Input
+                  wrapClassName="h-[150px] max-h-[250px] min-h-[100px] min-w-[920px] relative right-[45px]"
+                  className="input"
+                  name="field_Four"
+                  placeholder="Field 5"
+                ></Input>
               </div>
-              <div className="flex flex-row items-center justify-between ml-[auto] mt-[10px] md:w-[100%] w-[38%]">
-                <div
-                  className="common-pointer bg-white_A700 h-[20px] outline outline-[2px] outline-black_900 relative right-[200px] rounded-[50%] w-[20px]"
-                  onClick={() => navigate("/programdetailsentrycoldprobe")}
-                ></div>
-                <div
-                  className="common-pointer bg-white_A700 h-[20px] outline outline-[2px] outline-black_900 relative right-[200px] rounded-[50%] w-[20px]"
-                  onClick={() => navigate("/programdetailsentrymasstrial")}
-                ></div>
-              </div>
+
+              
+              <RadioButtonNew className="flex flex-row items-start justify-start mt-[71px] md:w-[100%] w-[91%]" />
             </div>
           </div>
           <div className="flex md:flex-1 flex-col gap-[10px] justify-start md:w-[100%] w-[5%]">
@@ -104,11 +83,6 @@ const ProgramDetailsEntryPage = () => {
             >
               Logout
             </Text>
-            <Img
-              src="images/img_scrollbar.svg"
-              className="h-[373px] w-[auto]"
-              alt="scrollbar"
-            />
           </div>
         </div>
       </div>
